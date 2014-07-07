77aae346bf758714dfcfd9bfb18ded9e25d0690a
Piotr Kowalczyk <piotr@indeed.com>
2014-07-07 13:21:47 -0500

    Create python package skeleton.
    - Basic setup.py w/ alpha version
    - README.md intended for thorough documentation
    - Empty requirements.txt
    - 'proctor' package

---
+++ README.md
@@ -0,0 +1,4 @@
+# django-proctor
+
+TODO: intro, can be used in other frameworks, required config options,
+basic usage, etc.
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1a0'
+
+setup(
+    name='django-proctor',
+    version=version,
+)
