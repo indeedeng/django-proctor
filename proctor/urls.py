abb068ec599194ff2000dd422c61a6830b98ee69
Sarah Ellis <sellis@indeed.com>
2018-05-10 16:28:18 -0500

    DJANGO-286: remove private from proctor urls

--- proctor/urls.py
+++ proctor/urls.py
@@ -3,7 +3,7 @@ from .views import private
 
 
 urlpatterns = [
-   url(r'^private/showTestMatrix/', private.ShowTestMatrixView.as_view(), name='showtestmatrix'),
-   url(r'^private/proctor/show', private.ShowTestMatrixView.as_view(), name='proctor_showtestmatrix'),
-   url(r'^private/proctor/force/', private.ForceGroupsView.as_view(), name='forcegroups'),
+   url(r'^showTestMatrix/', private.ShowTestMatrixView.as_view(), name='showtestmatrix'),
+   url(r'^proctor/show', private.ShowTestMatrixView.as_view(), name='proctor_showtestmatrix'),
+   url(r'^proctor/force/', private.ForceGroupsView.as_view(), name='forcegroups'),
 ]
