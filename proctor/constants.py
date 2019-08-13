27a14283c84d111f9fb2185dddd11e6a883ab8fc
Jaime Guajardo <jguajardo@indeed.com>
2019-08-13 15:25:16 -0500

    LAB-10697: adding pipet retries and lowering timeout threshold

--- proctor/constants.py
+++ proctor/constants.py
@@ -4,3 +4,5 @@ from __future__ import absolute_import, unicode_literals
 API_METHOD_GROUPS_IDENTIFY = 'groups/identify'
 API_METHOD_PROCTOR_MATRIX = 'proctor/matrix'
 PROP_NAME_FORCE_GROUPS = 'prforceGroups'
+MAX_HTTP_TIMEOUT_SECONDS = 0.25
+MAX_HTTP_RETRIES = 4
